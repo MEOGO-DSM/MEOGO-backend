8e704579be8405fdc48fdba9bdbb7058d7689a17
lee na kyung <ni1722650@gmail.com>
2024-01-18 05:20:01 +0900

    🌿 :: Exception Handler

---
+++ src/main/java/com/example/meogobackend/global/error/exception/BaseException.java
@@ -0,0 +1,14 @@
+package com.example.meogobackend.global.error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode) {
+        super();
+        this.errorCode = errorCode;
+    }
+}
+
