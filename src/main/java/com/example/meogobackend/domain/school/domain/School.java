1dcb8ababccd9d64db1bcda22a36ad03c82b56cd
lee na kyung <ni1722650@gmail.com>
2024-01-18 05:23:22 +0900

    🌿 :: SchoolEntity

---
+++ src/main/java/com/example/meogobackend/domain/school/domain/School.java
@@ -0,0 +1,33 @@
+package com.example.meogobackend.domain.school.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "school_name", nullable = false, length = 30)
+    private String schoolName;
+
+    @Column(nullable = false, length = 300)
+    private String schoolText;
+
+    @Column(nullable = false)
+    private String createDate;
+
+    @Builder
+    public School (String schoolName, String schoolText, String createDate){
+        this.schoolName = schoolName;
+        this.schoolText = schoolText;
+        this.createDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy .MM .dd"));
+    }
+}
---
+++ src/main/java/com/example/meogobackend/domain/school/domain/repository/SchoolRepository.java
@@ -0,0 +1,8 @@
+package com.example.meogobackend.domain.school.domain.repository;
+
+import com.example.meogobackend.domain.school.domain.School;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SchoolRepository extends CrudRepository<School, Long> {
+
+}
