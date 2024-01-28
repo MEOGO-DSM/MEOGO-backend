2a8d8ba8db5cb8e01798d9cb96725d03ed16183d
velyn666 <ni1722650@gmail.com>
2024-01-29 03:45:04 +0900

    🌿 :: RefreshToken

---
+++ src/main/java/com/example/meogobackend/domain/user/domain/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.example.meogobackend.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class RefreshToken {
+
+    @Id
+    private String accountId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Builder
+    public RefreshToken(String refreshToken, String accountId) {
+        this.refreshToken = refreshToken;
+        this.accountId = accountId;
+    }
+}
